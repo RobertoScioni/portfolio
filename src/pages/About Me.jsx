b2c827422c2dde678b0101b62325c36e72c2d7a9
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2021-04-13 22:45:50 +0200

    -

--- src/pages/About Me.jsx
+++ src/pages/About Me.jsx
@@ -4,13 +4,16 @@ const AboutMe = () => {
 			<p className="font-bold border-b-2 border-green-400 w-max text-green-400">
 				I'm a FullStack Web developer, and all-round IT enthusiast.
 			</p>
-			<pre className="ml-4">
-				{`I have a passion for all that is IT, but my main area of interest and
-expression has always been coding. Be it a bash script to map my
-keyboard’s extra key’s from F13 to F24, or bug fixing an old ASP
-application that a client bought in the ‘90s to make it work under
-Windows 10, code is where I always felt more at ease.`}
-			</pre>
+			<p className="ml-4">
+				I have a passion for all that is IT, but my main area of interest and
+				expression has always been coding.
+				<br />
+				Be it a BASH script to map my keyboard’s extra key’s from F13 to F24, or
+				bug fixing an old ASP application that a client bought in the ‘90s to
+				make it work under
+				<br />
+				Windows 10, code is where I always felt more at ease.
+			</p>
 			<p className="ml-4">
 				Recently, I Picked up few new skills and started this portfolio.
 			</p>
