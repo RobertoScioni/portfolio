e9cab4512a2ea3827d14ebaa59d621fc4d95ebea
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2021-04-13 19:17:46 +0200

    Update Argo.jsx

--- src/pages/Argo.jsx
+++ src/pages/Argo.jsx
@@ -2,14 +2,14 @@ const Argo = () => {
 	return (
 		<div className="flex flex-col">
 			<header>
-				<h3 className=" text-2xl">Argo</h3>
+				<h3 className=" text-2xl text-green-400">Argo</h3>
 				<h5 className=" ml-10 text-xl">
 					Responsive Web Application to manage roleplaying games.
 				</h5>
 			</header>
 			<br />
 			<article>
-				<h5 className=" text-xl">Features</h5>
+				<h5 className=" text-xl text-green-400">Features</h5>
 				<ul className=" ml-10 list-disc">
 					<li>Integrated support for FATE CORE RPG.</li>
 					<li>
